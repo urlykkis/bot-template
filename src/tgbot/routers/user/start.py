b1f77abd8e88018a5fb1dba44b16381213ab8354
urlykkis <killan_to@mail.ru>
2023-12-06 15:14:49 +0300

    updates

--- src/tgbot/routers/user/start.py
+++ src/tgbot/routers/user/start.py
@@ -3,7 +3,6 @@ from aiogram.filters import Command
 from aiogram.types import Message
 from fluentogram import TranslatorRunner
 
-from src.tgbot.common.keyboards.inline.menu import menu_markup
 
 router = Router(name="start")
 
@@ -11,5 +10,5 @@ router = Router(name="start")
 @router.message(Command("start"))
 async def command_start(message: Message, i18n: TranslatorRunner):
     return await message.answer(
-        i18n.user.start(), reply_markup=menu_markup()
+        i18n.user.start()
     )
